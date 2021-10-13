0347d375a0b209977c21f542c08b4cdc60dc6e7f
TAMIM <saifur.tamim@gmail.com>
2021-10-13 16:26:06 +0600

    checkout form
    checkout form created

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from '../AuthContext/AuthContext';
 import './Checkout.css';
 
 const Checkout = () => {
+    const { user } = useContext(UserContext);
     return (
         <div id="checkout-container">
             <h1 id="checkout-heading"><span>Checkout</span></h1>
+            <form>
+                <div className="input-field-container">
+                    <input type="text" value={user.displayName && user.displayName} placeholder="" />
+                    <label>Name</label>
+                </div>
+                <div className="input-field-container">
+                    <input type="email" value={user.email && user.email} placeholder="" />
+                    <label>Email</label>
+                </div>
+                <div className="input-field-container">
+                    <textarea cols="30" rows="10" placeholder="Address"></textarea>
+                    <label>Address</label>
+                </div>
+            </form>
         </div>
     );
 };
